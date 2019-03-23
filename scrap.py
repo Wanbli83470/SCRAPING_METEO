bc2c490c98fc7c97913cbf1464215a2462c87ee6
thomas83470 <estival.t@hotmail.com>
2019-03-23 13:34:05 +0100

    fonctionnement ok

--- scrap.py
+++ scrap.py
@@ -1,12 +1,15 @@
 import requests as r
 from bs4 import BeautifulSoup as b
 
-requête = r.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+
+
+requête = r.get("http://www.meteofrance.com/previsions-meteo-france/paris-15e-arrondissement/75015")
 html = requête.content
 
 soup = b(html, 'html.parser')
 
-seven_day = soup.find(id="seven-day-forecast")
-forecast_items = seven_day.find_all(class_="tombstone-container")
-today = forecast_items[0]
-print(today.text)
+temp_day = soup.select(".day-summary-temperature")[0]
+ciel = soup.select(".day-summary-label")[0]
+print(temp_day.text)
+print(ciel.text)
+
